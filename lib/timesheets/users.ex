aa5e92ac7b6ad3ab830f62914ccf2172213d91dd
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-10-31 10:41:36 -0400

    Add user session

--- lib/timesheets/users.ex
+++ lib/timesheets/users.ex
@@ -101,4 +101,10 @@ defmodule Timesheets.Users do
   def change_user(%User{} = user) do
     User.changeset(user, %{})
   end
+
+  def get_user_by_email(email) do
+    Repo.get_by(User, email: email)
+  end
+
+  def get_user(id), do: Repo.get(User, id)
 end
