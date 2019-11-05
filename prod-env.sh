726d8256fa8a23deaf29452f3828d5bcf32d3673
L Q <lingmiaojiayou@gmail.com>
2019-11-05 16:42:51 +0000

    deployment script

---
+++ prod-env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export MIX_ENV=prod
+export PORT=5002
+export DATABASE_URL="ecto://timesheets:us1Xai7aighe@localhost/timesheets_prod"
+export SECRET_KEY_BASE="/C9xG8nx/sbcqC8YZeygL6Q4Z5XAQPDXvlDmuIJTE/+8jEcSX4oQz7uAS3lu7x0c"
